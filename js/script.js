c6a6846e98c010384ab0b3ec1b607b4a9962bc10
Agnieszka Iwanaszko <a.iwanaszko@gmail.com>
2019-02-22 13:37:09 +0100

    Set player move

--- js/script.js
+++ js/script.js
@@ -18,3 +18,34 @@ if (randomNumber == '1') {
 }
 highlightBlock('s,:*4I!(;CZB-g(y[0S@');
 printMessage(('Mój ruch: ' + computerMove));
+
+
+var playerMove, playerInput;
+
+
+highlightBlock('~`Ep/l$#PjmD!dUeUpL.');
+playerInput = window.prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+highlightBlock('mu~TyzqPG_k]oEIN@73j');
+console.log(('Wpisana odpowiedź to: ' + playerInput));
+highlightBlock('v0?FGL$cTC]ShO!Mz-r7');
+if (playerInput == '1') {
+  highlightBlock('9Moipc!`=fIYWCzA1M3R');
+  playerMove = 'kamień';
+  highlightBlock('DNg(c|}aL94X12.T6|1X');
+  printMessage(('Twój ruch: ' + playerMove));
+} else if (playerInput == '2') {
+  highlightBlock('F8VIe%M4A(hvUr%gK[hh');
+  playerMove = 'papier';
+  highlightBlock('4Ls5o*PD6HlYGRqs1KyV');
+  printMessage(('Twój ruch: ' + playerMove));
+} else if (playerInput == '3') {
+  highlightBlock('B=I.JrGmUy)ur^Qyz]R7');
+  playerMove = 'nozyce';
+  highlightBlock(';+rSHE*bnx|@,;|R947s');
+  printMessage(('Twój ruch: ' + playerMove));
+} else {
+  highlightBlock('Lq@~F(Q`dfTzjq]X)+`?');
+  playerMove = 'nieznany ruch';
+  highlightBlock('wvH7lcd?4x#fC8Qr?HMC');
+  printMessage(('Error! Wpisz 1, 2 albo 3.' + ''));
+}
