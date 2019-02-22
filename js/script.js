138731516edb9f9f5fcaa2107c4120545445563f
Agnieszka Iwanaszko <a.iwanaszko@gmail.com>
2019-02-22 12:40:40 +0100

    Add basic js code

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var computerMove, playerMove;
+
+
+highlightBlock('Mhaz}dPies%2|2Jz#XEO');
+computerMove = 'kamień';
+highlightBlock('rw}O3R,Wh])%.ZZBm=28');
+printMessage(('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!'));
+
+highlightBlock('t=-?8}O3/^BA0Kk#z[V7');
+playerMove = 'papier';
+highlightBlock('`R}sEFtE=Cnxj[@j:0d)');
+printMessage(('Zagralem ' + playerMove + '! Jesli twoj ruch to nozyce, to wygrywasz!'));
